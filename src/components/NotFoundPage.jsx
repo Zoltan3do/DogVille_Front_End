910866a092e794d3482cb67ce9307f6d4707c36f
Manuel Barone <baronemanuel05@gmail.com>
2025-01-06 00:51:24 +0100

    update notfoundpage

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,6 +1,6 @@
 function NotFoundPage() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-dark text-light text-center ml-5 ">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-dark text-light text-center ">
             <div>
                 <h1 className="text-9xl font-bold">404</h1>
                 <p className="text-2xl">Page Not Found</p>
